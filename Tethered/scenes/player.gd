b23367d34571b831f5bd4df3decc56006cf2e495
ci0cc0lat0 <ci0cc0lat0@yahoo.com>
2021-06-12 18:47:06 -0500

    Fixing the state machine

--- Tethered/scenes/player.gd
+++ Tethered/scenes/player.gd
@@ -5,26 +5,54 @@ export (int) var maxSpeed = 300
 export (int) var gravity = 35
 export (int) var jumpForce = 600
 export (int) var maxFallSpeed = 1000
+var staticSword = preload("res://scenes/StaticBody2D.tscn")
+var SwordInstance = null
 var up = Vector2(0,-1)
 var velocity = Vector2.ZERO
 var facingRight = true
-
+var isSpawned = false
+var isIdle = true
 var STATE = UIMODE
 var LASTSTATE = 0
-enum {UIMODE, HEALMODE,SWORDMODE, SHIELDMODE, PLACEMODE, THROWMODE}
+
+enum {UIMODE, HEALMODE,SWORDMODE, IDLE, SHIELDMODE, PLACEMODE, THROWMODE, ATTACKMODE}
 
 func stateChoice():
-	if Input.is_action_just_pressed("heal"):
+	if Input.is_action_just_pressed("heal") && isSpawned == false:
 		STATE = HEALMODE
 		print(STATE)
+		$AnimationPlayer.play("SwordShove")
+		isSpawned == true	
 	if Input.is_action_just_pressed("attackMode"):
 		STATE = SWORDMODE
 		print(STATE)
-	if Input.is_action_just_pressed("shield"):
-		STATE = SHIELDMODE
+		if isSpawned == true:
+			SwordInstance.queue_free()
+		isSpawned = false
+	if Input.is_action_just_pressed("attack") && STATE == SWORDMODE :
 		print(STATE)
+		print("debug")
+		$AnimationPlayer.play("attack")
+		
+	#if Input.is_action_just_pressed("shield"):
+		#STATE = SHIELDMODE
+		
+		#isSpawned = false
+		#print(STATE)
 	
-
+func staticSwordSpawn():
+	
+	if facingRight == true:
+		isSpawned = true
+		SwordInstance = staticSword.instance()
+		SwordInstance.position = get_node("SwordShoveSpawnLeft").get_global_position()
+		get_parent().add_child(SwordInstance)
+	else:
+		isSpawned = true
+		SwordInstance = staticSword.instance()
+		SwordInstance.position = get_node("SwordShoveSpawnRight").get_global_position()
+		get_parent().add_child(SwordInstance)
+	
 func updateUI():
 	var hitbox = get_node("/root/mainscene/player/heath/hitbox")
 	var health_bar = get_node("/root/mainscene/player/heath/heathBar")
@@ -51,6 +79,7 @@ func realMovement(delta):
 	velocity.y += + gravity
 	if velocity.y > maxFallSpeed:
 		velocity.y = maxFallSpeed
+	
 	if Input.is_action_pressed("right"):
 		facingRight = true
 		velocity.x += acceleration
@@ -61,7 +90,9 @@ func realMovement(delta):
 		$AnimationPlayer.play("move")
 	else:
 		velocity.x = lerp(velocity.x,0,0.2)
-		$AnimationPlayer.play("idle")
+		STATE = IDLE
+		
+		
 	if is_on_floor():
 		if Input.is_action_just_pressed("jump"):
 			velocity.y = -jumpForce
@@ -71,6 +102,17 @@ func realMovement(delta):
 	velocity = move_and_slide(velocity,up)
 	
 func _physics_process(delta):
+	#print(isSpawned)
 	stateChoice()
 	realMovement(delta)
+	
+
+
+func _on_AnimationPlayer_animation_finished(anim_name):
+	if anim_name == "SwordShove":
+		staticSwordSpawn()
+		$AnimationPlayer.play("idle")
+	if anim_name == "attack":
+		#staticSwordSpawn()
+		$AnimationPlayer.play("idle")
 	
